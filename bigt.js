fdbe6b8fdc53344ef7e2ad1fe284e1cd3c942d84
Mark Doffman <mark.doffman@gmail.com>
2011-09-06 22:58:23 -0500

    Add the beginnings of a `Test` object.

--- bigt.js
+++ bigt.js
@@ -1,5 +1,6 @@
 var assert = require("assert");
 var console = require("console");
+var events = require("events");
 
 var merge = function (dest, source) {
     for (var p in source) {
@@ -35,18 +36,18 @@ var update = function(node, path, status) {
         // If referring to a descendant node
         var pathIndex = path.shift();
 
-        for (i=0; i<node.children.length; i++) {
-            var testIndex = node.children[i].index;
-            if (pathIndex >= testIndex)
+        var i=0;
+        for (; i<node.children.length; i++) {
+            if (node.children[i].index >= pathIndex)
                 break;
         }
 
-        if (i == pathIndex) {
-            // If the child path exists
+        if (node.children[i] && node.children[i].index == pathIndex) {
+            // If a matching node was found
             update(node.children[i], path, status);
         } else {
-            // Create it
-            node.children.splice(i+1, 0, update(merge(new StatusNode, {index: pathIndex}), path, status));
+            // Create a new desendant node, insert and update it
+            node.children.splice(i, 0, update(merge(new StatusNode, {index: pathIndex}), path, status));
         }
     }
 
@@ -68,6 +69,28 @@ assert.deepEqual(root, {
     error: null,
     index: 0,
     children: []
+});
+
+update(root, [1], {
+    name: "child-1",
+    path: [1],
+    status: "running",
+    error: null
+});
+assert.deepEqual(root, {
+    name: "root",
+    path: [],
+    status: "running",
+    error: null,
+    index: 0,
+    children: [{
+        name: "child-1",
+        path: [1],
+        status: "running",
+        error: null,
+        index: 1,
+        children: []
+    }]
 });
 
 update(root, [1], {
@@ -187,15 +210,17 @@ var lout = function(output, line, depth, color) {
     output.write(Array(depth+1).join("\t") + "\033[2K\033[" + color + "m" + line + "\n\033[37m");
 };
 
+
 var render = function(output, node, depth) {
     var lines = 1;
     lout(output, node.name, depth, color(node.status));
 
-    if(node.status == "failed" && node.err)
-        node.err.msg.split(/\r\n|\r|\n/).forEach(function(l) {
+    if(node.status == "failed" && node.error) {
+        node.error.stack.split(/\r\n|\r|\n/).forEach(function(l) {
             lines += 1;
             lout(output, l, depth+1, 37)
         });
+    }
     node.children.forEach(function(c) {
         lines += render(output, c, depth+1)
     });
@@ -206,5 +231,79 @@ var render = function(output, node, depth) {
 var sb = new StringBuffer();
 
 render(sb, root, 0);
+
+process.stdout.write(sb.toString());
+
+var Test = function (name, test) {
+    events.EventEmitter.call(this);
+
+    this.name = name;
+    this.test = test;
+    this.children = 0;
+};
+Test.prototype = Object.create(events.EventEmitter.prototype, {
+    constructor: {
+        value: Test, 
+        enumerable: false,
+        writable: true,
+        configurable: true
+    }
+});
+
+Test.prototype.run = function() {
+    try {
+        this.emit("status", merge(new StatusSignal(), {
+            name: this.name
+        }));
+        this.test(this);
+        this.emit("status", merge(new StatusSignal(), {
+            name: this.name,
+            status: "passed",
+        }));
+    } catch (err) {
+        this.emit("status", merge(new StatusSignal(), {
+            name: this.name,
+            status: "failed",
+            error: err
+        }));
+    }
+    return this;
+};
+
+Test.prototype.createChild = function(name, test) {
+    var self = this;
+    var child = new Test(name, test);
+    child.addListener("status", function(status) {
+        status.path.push(self.children);
+        self.emit("status", status);
+    });
+    self.children += 1;
+    return child;
+};
+
+var rootTest = new Test("Root Test", function(T) {
+    assert.ok(true);
+    T.createChild("S1", function(T) {
+        assert.ok(true);
+    }).run();
+    T.createChild("S2", function(T) {
+        assert.ok(true);
+    }).run();
+    T.createChild("S3", function(T) {
+        assert.ok(false);
+    }).run();
+});
+
+var rootNode = new StatusNode();
+
+rootTest.addListener("status", function(status) {
+    update(rootNode, status.path.slice(0), status);
+});
+
+rootTest.run();
+
+var sb = new StringBuffer();
+
+render(sb, rootNode, 0);
 
 process.stdout.write(sb.toString());
